8e1ffc012fa4403e38ceecb15fb5717490616067
unknown <assdfgzxcv4@gmail.com>
2021-04-25 23:37:13 +0800

    compatible with x64

--- iat_hook.c
+++ iat_hook.c
@@ -1,39 +1,53 @@
 
 #include<windows.h>
 
-int set_iat_hook()
+int set_iat_hook(const char* dll_name, const char* api_name, void** addr)
 {
 	HMODULE image_base = GetModuleHandle(NULL);
-	IMAGE_DOS_HEADER* dos = image_base;
-	IMAGE_NT_HEADERS32* nt = (DWORD)image_base + dos->e_lfanew;
-	IMAGE_IMPORT_DESCRIPTOR* import = (DWORD)image_base + nt->OptionalHeader.DataDirectory[1].VirtualAddress;
-	DWORD import_size = nt->OptionalHeader.DataDirectory[1].Size;
+	PIMAGE_DOS_HEADER dos = image_base;
+	PIMAGE_NT_HEADERS nt = (char*)image_base + dos->e_lfanew;
+	PIMAGE_IMPORT_DESCRIPTOR import = (char*)image_base + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+	DWORD import_size = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size;
 	BOOL flag = FALSE;
-	DWORD* import_fun = NULL;
+	void** iat = NULL;
 	while (import->FirstThunk != NULL && flag == FALSE)
 	{
-		//char* a = (DWORD)image_base + import->Name;
-		import_fun = (DWORD)image_base + import->FirstThunk;
-		if (strcmp((DWORD)image_base + import->Name, "KERNEL32.dll") == 0)
+		iat = (char*)image_base + import->FirstThunk;
+		if (stricmp((char*)image_base + import->Name, dll_name) == 0)
 		{
-			IMAGE_THUNK_DATA32* thunk = (DWORD)image_base + import->OriginalFirstThunk;
-			while (thunk)
+			PIMAGE_THUNK_DATA thunk = (char*)image_base + import->OriginalFirstThunk;
+			while (thunk->u1.AddressOfData)
 			{
-				IMAGE_IMPORT_BY_NAME* import_by_name = (DWORD)image_base + thunk->u1.ForwarderString;
-				
-				if (strcmp(import_by_name->Name, "MessageBoxA") == 0)
+				IMAGE_IMPORT_BY_NAME* import_by_name = (char*)image_base + thunk->u1.ForwarderString;
+				if (IMAGE_SNAP_BY_ORDINAL(thunk->u1.Ordinal))
 				{
-					flag = TRUE;
-					DWORD old_protect;
-					VirtualProtect(import_fun, sizeof(DWORD), PAGE_READWRITE, &old_protect);
-					DWORD x = *import_fun = 0x00; //在这修改；
-					VirturalProtect(import_fun, sizeof(DWORD), old_protect, 0);
-					break;
+					if (IMAGE_ORDINAL(thunk->u1.Ordinal) == (SIZE_T)api_name)
+					{
+						flag = TRUE;
+						DWORD old_protect;
+						VirtualProtect(iat, sizeof(void*), PAGE_READWRITE, &old_protect);
+						*addr = InterlockedExchangePointer(*iat, *addr);
+						VirtualProtect(iat, sizeof(void*), old_protect, 0);
+						break;
+					}
+				}
+				else
+				{
+					if ((strcmp(import_by_name->Name, api_name) == 0))
+					{
+						flag = TRUE;
+						DWORD old_protect;
+						VirtualProtect(iat, sizeof(void*), PAGE_READWRITE, &old_protect);
+						*addr = InterlockedExchangePointer(iat, *addr);
+						VirtualProtect(iat, sizeof(void*), old_protect, 0);
+						break;
+					}
+
 				}
 				thunk++;
-				import_fun++;
+				iat++;
 			}
-			
+
 		}
 		import++;
 	}
